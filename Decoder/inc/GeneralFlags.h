6b28c2a274c7e490d1abaeb0266c63f92b9f438c
aloloap <aloaizapereira@gmail.com>
2018-04-20 19:03:05 -0500

    Added general flags header

---
+++ Decoder/inc/GeneralFlags.h
@@ -0,0 +1,19 @@
+//***************************************************************************
+//* FileName:       GeneralFlags.h
+//*
+//* Description:    Defines specific defines for specific compilers and others.
+//*
+//***************************************************************************
+
+#ifndef COMPILER_H_
+#define COMPILER_H_
+
+#define MSVC_COMPILER 1
+#define GCC_COMPILER  0
+
+#define SUCCESS  0
+#define FAILURE -1
+
+#define C55X 0
+
+#endif
